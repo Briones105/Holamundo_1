9a08e42b124bbc640c73bfdb0d0b509147d5c5de
Briones105 <79547034+Briones105@users.noreply.github.com>
2021-02-23 16:54:50 -0600

    Add files via upload

---
+++ holamundo.c
@@ -0,0 +1,16 @@
+/*PROGRAMA Holamundo
+  briones ramirez alejandro
+  programa que imprime una cadena de caracteres
+*/
+//archivos de cabecera
+#include<stdio.h>
+//contantes o bariables 
+//definicion e implementacion del programa principal
+int main()
+{
+	printf("HOLA MUNDO \n");
+	printf("MI NOMBRE ES ALEJANDRO BRIONES RAMIREZ");
+	return 0;
+	getch ();
+}
+
